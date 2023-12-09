ac325c704ce0260c7c6c709e44421aa5a2912ab2
Muhammad Sohaib <muhammadsohaib800k@gmail.com>
2023-12-09 13:58:50 +0500

    Add files via upload

---
+++ lab13-AVL Trees/AVL Trees - All Operations C++ Code.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+	public:
+    	int key;
+    	Node* left;
+    	Node* right;
+    	int height;
+
+    	Node(int value) {
+        	key = value;
+        	left = NULL;
+        	right = NULL;
+        	height = 1;
+    	}
+};
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int height(Node* node) {
+    if (node == NULL)
+        return 0;
+    return node->height;
+}
+
+int getBalance(Node* node) {
+    if (node == NULL)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+Node* insert(Node* root, int key) {
+    if (root == NULL)
+        return new Node(key);
+
+    if (key < root->key)
+        root->left = insert(root->left, key);
+    else if (key > root->key)
+        root->right = insert(root->right, key);
+    else
+        return root; // Duplicate keys are not allowed in AVL tree
+
+    root->height = 1 + max(height(root->left), height(root->right));
+
+    int balance = getBalance(root);
+
+    // Left Left Case
+    if (balance > 1 && key < root->left->key)
+        return rightRotate(root);
+
+    // Right Right Case
+    if (balance < -1 && key > root->right->key)
+        return leftRotate(root);
+
+    // Left Right Case
+    if (balance > 1 && key > root->left->key) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Left Case
+    if (balance < -1 && key < root->right->key) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+Node* findMin(Node* node) {
+    while (node->left != NULL)
+        node = node->left;
+    return node;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL)
+        return root;
+
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else {
+        // Node with one child or no child
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        // Node with two children
+        Node* temp = findMin(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+
+    // Update height and balance factor
+    root->height = 1 + max(height(root->left), height(root->right));
+
+    int balance = getBalance(root);
+
+    // Left Left Case
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+
+    // Left Right Case
+    if (balance > 1 && getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Right Case
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+
+    // Right Left Case
+    if (balance < -1 && getBalance(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+Node* search(Node* root, int key) {
+    if (root == NULL || root->key == key)
+        return root;
+
+    if (key < root->key)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+void inorderTraversal(Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        cout << root->key << " ";
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    // Insert elements into the AVL tree
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 5);
+    
+
+    cout << "Inorder traversal of the AVL tree: ";
+    inorderTraversal(root);
+    cout << "\n";
+
+    int keyToDelete = 20;
+    root = deleteNode(root, keyToDelete);
+
+    cout << "Inorder traversal after deletion: ";
+    inorderTraversal(root);
+    cout << "\n";
+
+    int keyToSearch = 50;
+    Node* searchResult = search(root, keyToSearch);
+
+    if (searchResult != NULL)
+        cout << keyToSearch << " found in the AVL tree.\n";
+    else
+        cout << keyToSearch << " not found in the AVL tree.\n";
+
+    return 0;
+}
+
